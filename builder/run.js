1665f08cfb9888ebd9b93e8f654096f83b90296b
Evan Welsh <contact@evanwelsh.com>
2021-04-27 22:20:00 -0700

    Allow running only specific package sets.

--- builder/run.js
+++ builder/run.js
@@ -1,15 +1,22 @@
-import { readFile, rmdir, rm } from 'fs/promises';
+import { readFile } from 'fs/promises';
 
 import { cloneRepo, installDependencies, buildTypes } from './pull.js';
 import { buildPackages } from './buildPackages.js';
 import { commitChangesFiles, hasModifiedPackageFiles } from './git.js';
 
 const { repos } = JSON.parse(await readFile('./repos.json', { encoding: 'utf-8' }));
-/** @type {{tag:string; path: string; }[]} */
-const paths = repos.map(repo => ({ tag: repo.tag ?? 'latest', path: `gi-ts/${repo.name}` }));
+/** @type {{tag:string; path: string; name: string; }[]} */
+const paths = repos.map(repo => ({ name: repo.name, tag: repo.tag ?? 'latest', path: `gi-ts/${repo.name}` }));
 
-for (const { path, tag } of paths) {
-    console.log(`Updating packages for: ${path}`);
+const selectedName = process.argv[2];
+
+for (const { path, tag, name } of paths) {
+    if (selectedName && name !== selectedName) {
+        console.log(`Skipping ${path}...`);
+        continue;
+    } else {
+        console.log(`Updating packages for: ${path}`);
+    }
 
     try {
         await cloneRepo(path);
